bba1cccb2351cddebae279cb762d0d94791b062a
wang xiaojun <wang_xj@suixingpay.com>
2013-12-02 11:15:02 +0800

    修改检测监听端口的问题

--- check_tcp_stat.sh
+++ check_tcp_stat.sh
@@ -9,15 +9,42 @@ STATE_UNKNOWN=3
 #help
 help () {
         echo -en "Usage: $0 -H <host> -p <port>\nFor example:\t$0 -H 192.168.0.6 -p 8819\n" 1>&2
-        exit ${STATE_UNKNOWN}
+        exit ${STATE_WARNING}
 }
 
 check_num () {
 	local num_str="$1"
 	echo ${num_str}|grep -E '^[0-9]+$' >/dev/null 2>&1 || stat='not a number!'
 	if [ "${stat}" = 'not a number!' ];then
-   		echo "${num_str} ${stat}"
-		help
+   		echo "${num_str} ${stat}" 1>&2
+		exit ${STATE_WARNING}
+	fi
+}
+
+check_ip () {
+	local ip_str="$1"
+	echo "${ip_str}"|grep -P '^\d{1,3}(\.\d{1,3}){3}$' >/dev/null 2>&1 || str_stat='not a ip!'
+	if [ "${ip_stat}" = 'not a ip!' ];then
+        echo "${ip_str} ${stat}" 1>&2
+		exit ${STATE_WARNING}
+    fi
+}
+
+check_state () {
+	local stat_str="$1"
+	if [ -n "${stat_str}" ];then
+		case "${stat_str}" in
+                TIME_WAIT|FIN_WAIT|ESTABLISHED|CLOSING|SYN_SEND|TIMED_WAIT)
+					cmd="netstat -nt|grep ${stat_str}"
+                ;;
+				LISTEN)
+					cmd="netstat -ntl"
+				;;
+                *)
+                    echo "This script not support ${stat_str}" 1>&2
+                    exit 1
+                ;;
+		esac
 	fi
 }
 
@@ -30,40 +57,39 @@ message () {
 while getopts w:c:p:H:S: opt
 do
         case "$opt" in
-		w) warning=$OPTARG;;
-        c) critical=$OPTARG;;
-        p) port="$OPTARG";;
-        H) ip="$OPTARG";;
-		S) state="$OPTARG";;
+		w) 
+			warning=$OPTARG;;
+        c) 
+			critical=$OPTARG;;
+        p) 
+			port="$OPTARG"
+			check_num "${port}"
+		;;
+        H) 
+			ip="$OPTARG"
+			check_ip "${ip}"
+		;;
+		S) 
+			state="$OPTARG"
+			check_state "${state}"
+		;;
         *) help;;
         esac
 done
 shift $[ $OPTIND - 1 ]
 
 [ $# -gt 0 -o -z "${warning}" -o -z "${critical}" ] && help
-
 [ ${warning} -gt ${critical} ] && echo "-w ${warning} must lower than -c ${critical}!" && exit ${STATE_UNKNOWN}
 
-[ -n "${port}" ] && check_num "${port}"
-
-if [ -n "${state}" ];then
-	case "${state}" in
-    	TIME_WAIT|FIN_WAIT|ESTABLISHED|CLOSING|SYN_SEND|TIMED_WAIT)
-			info=`netstat -tn|grep "${state}"|grep "${ip}:${port}"|\
-			awk 'BEGIN{OFS=": ";ORS=";"}{stats[$(NF)]+=1}END{for (stat in stats) {print stat,stats[stat];sum+=stats[stat]};print "Total",sum}'`
-    		;;
-    	*)
-        	echo "This script not support ${state}" 1>&2
-			exit 1
-        	;;
-	esac
-else
+if [ -z "${state}" ];then
 	info=`netstat -tn|grep "${ip}:${port}"|\
+	awk 'BEGIN{OFS=": ";ORS=";"}{stats[$(NF)]+=1}END{for (stat in stats) {print stat,stats[stat];sum+=stats[stat]};print "Total",sum}'`
+else
+	info=`eval "$cmd"|grep "${ip}:${port}"|\
 	awk 'BEGIN{OFS=": ";ORS=";"}{stats[$(NF)]+=1}END{for (stat in stats) {print stat,stats[stat];sum+=stats[stat]};print "Total",sum}'`
 fi
 
-#info=`netstat -tn|grep 'ESTABLISHED'|grep "${ip}:${port}"`
-[ -z "${info}" ] && message "UNKNOWN" && exit "${STATE_UNKNOWN}"
+echo "${info}"|grep -E '[0-9]' >/dev/null 2>&1 || eval "echo Info is empty! 1>&2;exit ${STATE_UNKNOWN}"
 
 min=0
 max=4096
