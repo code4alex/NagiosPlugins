37581c51e1b25b0ca03cf7f1dbf1b080560d5e8d
wang xiaojun <wang_xj@suixingpay.com>
2013-12-10 10:24:09 +0800

    添加check_lsof.sh的PNP4NAGIOS的php模板

---
+++ check_lsof.php
@@ -0,0 +1,23 @@
+<?php
+#
+# Copyright (c) 2006-2010 Joerg Linge (http://www.pnp4nagios.org)
+# Plugin: check_lsof.php
+#
+#
+$ds_name[1] = "Check lsof";
+$opt[1]  = "--lower-limit 0 --vertical-label \"Check lsof\"  --title \"Check lsof\" ";
+$def[1]  =  rrd::def("var1", $RRDFILE[1], $DS[1], "AVERAGE") ;
+$def[1] .=  rrd::gradient("var1", "ff5c00", "ffdc00", "Check lsof", 10) ;
+$def[1] .=  rrd::gprint("var1", array("LAST", "MAX", "AVERAGE"), "%.0lf $UNIT[1]") ;
+$def[1] .=  rrd::line1("var1", "#000000") ;
+
+if($WARN[1] != ""){
+        if($UNIT[1] == "%%"){ $UNIT[1] = "%"; };
+        $def[1] .= rrd::hrule($WARN[1], "#FFFF00", "Warning  ".$WARN[1].$UNIT[1]."\\n");
+}
+if($CRIT[1] != ""){
+        if($UNIT[1] == "%%"){ $UNIT[1] = "%"; };
+        $def[1] .= rrd::hrule($CRIT[1], "#FF0000", "Critical ".$CRIT[1].$UNIT[1]."\\n");
+}
+
+?>
