cac21d08942adfb0135a61eecf9d9b5f93b0d498
wang xiaojun <wang_xj@suixingpay.com>
2014-01-18 21:39:26 +0800

    修改显示输出和判断

--- check_bond.sh
+++ check_bond.sh
@@ -32,10 +32,24 @@ if [ ! -f ${dev} ];then
         usage
 fi
 
-bond_info=`grep -E 'Currently Active Slave|MII Status|Slave Interface' ${dev}|\
-awk 'BEGIN{ORS=";"}{print}'`
+#bond_info=`grep -E 'Currently Active Slave|MII Status|Slave Interface' ${dev}|\
+#awk 'BEGIN{ORS=";"}{print}'`
 
-bond_active=`echo "${bond_info}"|awk -F';|: ' '{print $2}'`
+bond_info=`grep -E 'Bonding Mode|Currently Active Slave|MII Status|Slave Interface' ${dev} |\
+awk -F':' '
+BEGIN{
+ORS=" "
+}
+NR==1{print "Mode:"$2";"}
+NR==2{print "Active:"$2}
+$1~/MII Status/{print "is"$2";"}
+$1~/Slave Interface/{print "Interface:"$2}
+'`
+
+#bond_active=`echo "${bond_info}"|awk -F';|: ' '{print $2}'`
+bond_active=`grep -E 'Currently Active Slave' ${dev} | awk '{print $NF}'`
+
+down_status=`echo ${bond_info}|awk 'BEGIN{RS=";";ORS=";"}/down/{print}'`
 
 nagios_lib='/usr/local/nagios/libexec'
 
@@ -45,13 +59,20 @@ if [ ! -f "${bond_mark}" ]; then
         echo ${bond_active} > ${bond_mark}
 fi
 
+test -f ${bond_mark} && chown nagios.nagios ${bond_mark}
+
 mark_value=`cat ${bond_mark}`
 
-if [ "${bond_active}" = ${mark_value} ];then
-        echo "${dev_id} is OK! ${bond_info}"
-        exit ${STATE_OK}
-else
+if [ "${bond_active}" != ${mark_value} ];then
         echo "${dev_id} has been changed! ${mark_value} --> ${bond_active}. ${bond_info}"
         echo "${bond_active}" > ${bond_mark}
         exit ${STATE_WARNING}
 fi
+
+if [ -n "${down_status}" ];then
+		echo "${down_status}. ${bond_info}"
+		exit ${STATE_WARNING}
+fi
+
+echo "${dev_id} is OK! ${bond_info}"
+exit ${STATE_OK}
