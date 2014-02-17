5a0632bba133b4aa0cb2db67a9863c317bd8e81c
wang xiaojun <wang_xj@suixingpay.com>
2014-02-17 10:46:35 +0800

    修改获取数据BUG,增强程序鲁棒性

--- check_net_traffic.sh
+++ check_net_traffic.sh
@@ -53,50 +53,59 @@ fi
 
 source_file='/proc/net/dev'
 if [ ! -f "${source_file}" ];then
-		echo "${source_file} not not exsit!" 1>&2
-		exit ${STATE_WARNING}
+                echo "${source_file} not not exsit!" 1>&2
+                exit ${STATE_WARNING}
 fi
 
 grep "${dev_id}" ${source_file} >/dev/null 2>&1 || dev_stat='not found'
 if [ "${dev_stat}" = 'not found' ];then
-		echo "${dev_id} ${dev_stat}!" 1>&2
-		usage
+                echo "${dev_id} ${dev_stat}!" 1>&2
+                usage
 fi
 
 time_now=`date -d now +"%F %T"`
 mark="/usr/local/nagios/libexec/net_traffic.${dev_id}"
-info=`awk -F':|[ ]+' -v date="${time_now}"  'BEGIN{OFS=";"}/'${dev_id}'/{print "TIME=\""date"\"","RX="$3,"TX="$11,"DEV='${dev_id}'"}' "${source_file}"`
+search_dev=`awk -F':' '/'${dev_id}'/{print $2}' /proc/net/dev|sed -r 's/^[ ]+//'`
+info=`echo "${search_dev}"|awk -v date="${time_now}"  'BEGIN{OFS=";"}{print "TIME=\""date"\"","RX="$1,"TX="$9,"DEV='${dev_id}'"}'`
 
 #debug
 #eval "${info}"
 #echo $info 
 #echo $TIME $RX $TX && exit
 
+marking () {
+        echo "$info" > ${mark} || exit ${STATE_WARNING}
+        chown nagios.nagios ${mark}
+}
+
 if [ ! -f "${mark}" ];then
-		echo "$info" > ${mark} || exit ${STATE_WARNING}
-		chown nagios.nagios ${mark}
-		echo "This script is First run! ${info}"
-		exit ${STATE_OK}
+                marking
+                echo "This script is First run! ${info}"
+                exit ${STATE_OK}
 else
-		old_info=`cat ${mark}`
-		eval "${old_info}"
-		OLD_TIME="${TIME}";OLD_RX=${RX};OLD_TX=${TX}		
+                old_info=`cat ${mark}`
+                eval "${old_info}"
+                OLD_TIME="${TIME}";OLD_RX=${RX};OLD_TX=${TX}
+                if [ -z "${OLD_RX}" -o -z "${OLD_TX}" ];then
+                        echo "Data Error: ${old_info}" 1>&2
+                        marking
+                        exit ${STATE_WARNING}
+                fi
 fi
 
 if [ -n "${info}" ];then
-		eval ${info}
-		sec_now=`date -d "${TIME}" +"%s"`
-		sec_old=`date -d "${OLD_TIME}" +"%s"`
-		sec=`echo "${sec_now}-${sec_old}"|bc`
-		rx=`echo "(${RX}-${OLD_RX})/${sec}"|bc`
-		tx=`echo "(${TX}-${OLD_TX})/${sec}"|bc`
-		echo "$info" > ${mark} || exit ${STATE_WARNING}
-		chown nagios.nagios ${mark}
+                eval ${info}
+                sec_now=`date -d "${TIME}" +"%s"`
+                sec_old=`date -d "${OLD_TIME}" +"%s"`
+                sec=`echo "${sec_now}-${sec_old}"|bc`
+                rx=`echo "(${RX}-${OLD_RX})/${sec}"|bc`
+                tx=`echo "(${TX}-${OLD_TX})/${sec}"|bc`
+                marking
 #debug
-#		echo $sec $rx $tx
+#               echo $sec $rx $tx
 else
-		echo "Can not read ${source_file}" 1>&2
-		exit ${STATE_WARNING}
+                echo "Can not read ${source_file}" 1>&2
+                exit ${STATE_WARNING}
 fi
 
 human_read () {
