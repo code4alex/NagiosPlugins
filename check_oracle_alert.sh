e9602f26ead735d38c655f9e7c753233411896e8
Alex wang <qq5910225@gmail.com>
2014-09-03 16:59:17 +0800

    添加check_oracle_alert.sh 脚本

--- check_oracle_alert.sh
+++ check_oracle_alert.sh
@@ -6,17 +6,17 @@ STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 
-file='/u01/app/oracle/admin/oa/bdump/alert_oa.log'
-#my_date=`date -d "-1 minute" +"%a %b %d %R"`
+file='/opt/oracle/logs/alert.log'
+
 export LC_ALL=C
-my_date=`date -d "-1 minute" +"%a %b %_d %R"`
+my_date=`date -d "-1 minute" +"%a %b %d %R"`
 
 if [ ! -e ${file} ];then
         echo "${file} not exist!"
         exit ${STATE_UNKNOWN}
 fi
 
-info=`mawk "BEGIN{a=0};/${my_date}/{a=NR};a != 0{print}" ${file}|\
+info=`tail -n 2000 ${file}|mawk "BEGIN{a=0};/${my_date}/{a=NR};a != 0{print}"|\
 grep -E 'ORA-|Error|WARNING|Starting|Shutting'|sort -u`
 
 
