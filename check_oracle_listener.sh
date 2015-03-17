aaeccc9f4d2feb05b29824dc5f688a1404a0ba53
Alex wang <qq5910225@gmail.com>
2015-03-17 14:41:36 +0800

    带入文件名参数

--- check_oracle_listener.sh
+++ check_oracle_listener.sh
@@ -6,13 +6,23 @@ STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
 
-file='/opt/oracle/logs/listener.log'
+usage () {
+        echo -en "USAGE: $0 /opt/oracle/logs/listener.log\n" 1>&2
+        exit ${STATE_WARNING}
+}
+
+file="$1"
+
+export LC_ALL=C
+
+#file='/opt/oracle/logs/listener.log'
 #my_date=`date -d "-1 minute" +"%d-%b-%Y %R"`
 my_date=`date -d "-1 minute" +"%a %b %d %R"`
 
 if [ ! -e ${file} ];then
         echo "${file} not exist!"
-        exit ${STATE_UNKNOWN}
+	usage
+#        exit ${STATE_UNKNOWN}
 fi
 
 info=`tail -n 2000 ${file}|mawk "BEGIN{IGNORECASE=1;a=0};/${my_date}/{a=NR};a != 0{print}"|\
