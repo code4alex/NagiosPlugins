6a388af486cc93e455a371335d89f67c1b93b703
xiaojun.wang <xiaojun.wang@advance.ai>
2019-03-14 10:57:11 +0800

    fix

--- check_rabbitmq_queue_influx.sh
+++ check_rabbitmq_queue_influx.sh
@@ -19,7 +19,7 @@ trap "test -f ${tmp_file} && rm -f ${tmp_file}"  EXIT
 influx -database telegraf -format csv \
 -execute 'SELECT mean("messages") FROM "rabbitmq_queue" WHERE time > now() -1m GROUP BY time(10s), "queue", "node" fill(none)'|\
 grep -Ev '^name'|\
-mawk -F',' -v value=${warn} '$NF>value{print $(NF-3),$(NF-2),$NF}'|\
+mawk -F',' '$NF>0{print $(NF-3),$(NF-2),$NF}'|\
 sort -u|sed 's/"//g'|\
 while read node queue num
 do
@@ -27,14 +27,23 @@ do
     echo "${node} ${queue} ${num}"
 done > ${tmp_file}
 
-if [ -s "${tmp_file}" ];then
+[ ! -s "${tmp_file}" ] && echo "Check rabbitmq_queue is OK!|num=0;;;" && exit ${STATE_OK}
+
+number=`mawk '{stats[$1" "$2]+=$3;count[$1" "$2]+=1}END{for (stat in stats) {sum+=stats[stat]/count[stat]};print sum}' ${tmp_file}|xargs -r -i echo "{}/1"|bc`
+echo ${number}
+
+if [ ${number} -ge ${warn} ];then
     echo "Check rabbitmq_queue is WARNING!"
-    cat ${tmp_file}
-    number=`cat ${tmp_file}|mawk '{sum+=$NF}END{print sum}'`
+    mawk '{stats[$1" "$2]+=$3;count[$1" "$2]+=1}END{for (stat in stats) {print stat,stats[stat]/count[stat]}}' ${tmp_file}|\
+    while read node queue value
+    do
+        num_int=`echo ${value}/1|bc`
+        echo "$node $queue $num_int"
+    done|sort -nrk3
     echo "|num=${number};;;"
     exit ${STATE_WARNING}
 else
-    echo "Check rabbitmq_queue is OK!|num=0;;;" && exit ${STATE_OK}
+    echo "Check rabbitmq_queue is OK!|num=${number};;;" && exit ${STATE_OK}
 fi
 
 exit ${STATE_UNKNOWN}
