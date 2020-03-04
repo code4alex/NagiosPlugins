e1daabc6a64c6a9603e383b3acdd22dd8adb09ff
Alex Wong <5910225@qq.com>
2020-03-04 16:30:14 +0800

    add

---
+++ check_rabbitmq_queue_influx_new.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+warn="$1"
+
+path=`pwd`
+file=`echo $0|sed 's/.//'`
+test -z "${warn}" &&\
+eval "echo please enter: ${path}${file} 10000;exit ${STATE_UNKNOWN}"
+
+tmp_file="/tmp/mq.stat.$$"
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap "test -f ${tmp_file} && rm -f ${tmp_file}"  EXIT
+
+#-execute 'SELECT mean("messages") FROM "rabbitmq_queue" WHERE time > now() -1m GROUP BY time(30s), "queue", "node" fill(none)'|\
+influx -host 127.0.0.1 -username telegraf -password 123456 -database telegraf -format csv \
+-execute 'SELECT mean("messages") FROM "rabbitmq_queue" WHERE "host" =~ /prod-genie-mq1/ AND time > "now() -1m" GROUP BY time(1m), "queue", "node" fill(none)'|\
+grep -Ev '^name'|\
+mawk -F',' '$NF>0{print $(NF-3),$(NF-2),$NF}'|\
+sort -u|sed 's/"//g'|\
+while read node queue num
+do
+    eval "${node};${queue}"
+    echo "${node} ${queue} ${num}"
+done > ${tmp_file}
+
+[ ! -s "${tmp_file}" ] && echo "Check rabbitmq_queue is OK!|num=0;;;" && exit ${STATE_OK}
+
+number=`mawk '{stats[$1" "$2]+=$3;count[$1" "$2]+=1}END{for (stat in stats) {sum+=stats[stat]/count[stat]};print sum}' ${tmp_file}|xargs -r -i echo "{}/1"|bc`
+#echo ${number}
+
+if [ ${number} -ge ${warn} ];then
+    echo "Check rabbitmq_queue is WARNING!"
+    mawk '{stats[$1" "$2]+=$3;count[$1" "$2]+=1}END{for (stat in stats) {print stat,stats[stat]/count[stat]}}' ${tmp_file}|\
+    while read node queue value
+    do
+        num_int=`echo ${value}/1|bc`
+        echo "$node $queue $num_int"
+    done|sort -nrk3|head -n10
+    echo "|num=${number};;;"
+    exit ${STATE_WARNING}
+else
+    echo "Check rabbitmq_queue is OK!${number}/min|num=${number};;;" && exit ${STATE_OK}
+fi
+
+exit ${STATE_UNKNOWN}
